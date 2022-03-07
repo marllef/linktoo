a03f0a588ae13b51c719f52740157b0083cd09d5
Marllef H A Freitas <marllef-alves@hotmail.com>
2022-03-06 22:48:38 -0300

    fix(LinkPanel): Removido imports não utilizados

--- src/components/Panel/LinkPanel/index.tsx
+++ src/components/Panel/LinkPanel/index.tsx
@@ -1,12 +1,7 @@
-import { Box, Button, Center } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { FaPlus } from "react-icons/fa";
+import { Box } from "@chakra-ui/react";
 import { CreateAction } from "~/components/Actions/Create";
 import { ListLink } from "~/components/List/ListLink";
 import { useLinks } from "~/hooks/fetcher";
-import styles from "./LinkPanel.module.css";
-
-interface Props {}
 
 export const LinkPanel = () => {
   const { links } = useLinks();
