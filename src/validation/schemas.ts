186fc622ae6174f2f45cd502e2f161dd95fc86c7
Marllef H A Freitas <marllef-alves@hotmail.com>
2022-03-13 18:11:12 -0300

    fix(validation/schema): Ajustando validação

--- src/validation/schemas.ts
+++ src/validation/schemas.ts
@@ -1,11 +1,7 @@
 import * as yup from "yup";
 
 export const LinkSchema = yup.object().shape({
-  index: yup
-    .number()
-    .positive("A posição tem que ser positiva.")
-    .optional()
-    .default(0),
+  index: yup.number().default(0),
   title: yup.string().required("O titulo é obrigatório."),
   href: yup.string().required("O link é obrigatório."),
 });
