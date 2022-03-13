b1cb803ba4f561063c2a0f24901d9b47ab16ba54
Marllef H A Freitas <marllef-alves@hotmail.com>
2022-03-13 15:31:01 -0300

    feat(Brand): Adicionando push to home

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -1,8 +1,13 @@
+import { useRouter } from "next/router";
 import { FaShareAlt } from "react-icons/fa";
 
 export const Brand = () => {
+  const router = useRouter();
   return (
-    <div className="flex text-xl sm:text-lg items-center select-none">
+    <div
+      className="flex text-xl sm:text-lg items-center cursor-pointer select-none"
+      onClick={() => router.push("/")}
+    >
       <FaShareAlt className="text-sky-500" />
       <div className="font-bold">Link</div>
       <div className="text-sky-500">Too</div>
